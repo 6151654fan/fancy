539a5fc9b2118ad150ebbf1402298fb2cb08730b
fancy <294321232@qq.com>
2026-03-30 20:59:44 +0800

    用户数据保存本地

---
+++ app/types/better-sqlite3.d.ts
@@ -0,0 +1,16 @@
+declare module "better-sqlite3" {
+  class Database {
+    constructor(filename: string, options?: any);
+    exec(sql: string): void;
+    prepare(sql: string): Statement;
+    transaction<T>(callback: () => T): T;
+  }
+
+  class Statement {
+    all(...params: any[]): any[];
+    get(...params: any[]): any;
+    run(...params: any[]): { changes: number; lastInsertRowid: number };
+  }
+
+  export default Database;
+}
